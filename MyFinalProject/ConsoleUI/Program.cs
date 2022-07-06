967323b4a0e4760b1610b13ad6326dd26bfd4d85
HuseyinOzlu <hozlu509@gmail.com>
2022-07-06 13:33:14 +0300

    new commit

---
+++ MyFinalProject/ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsolUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        
+     }
+}
